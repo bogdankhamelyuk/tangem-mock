ebcce36e937aa8c1e121764e3d8f97580f6e47c8
Bogdan Khamelyuk <bogdankhamelyuk@gmail.com>
2024-01-10 23:43:34 +0100

    added scroll effect on the bottom bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -12,5 +12,13 @@ import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  bottomBarOpacity: number = 0;
   title = 'tangem-mock';
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Adjust the opacity based on the scroll position
+    this.bottomBarOpacity = Math.min(scrollPosition / 200, 1); 
+  }
 }
